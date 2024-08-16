2ec737a5104c864655b311afc1f7a0b0a9eb2abd
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-08-16 17:21:53 +0300

    bug fix and crud in frontend is end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,10 +10,10 @@ export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/books/create' element={<CreateBooks/>} />
-      <Route path='/books/show' element={<ShowBooks/>} />
-      <Route path='/books/edit' element={<EditBooks/>} />
-      <Route path='/books/delete' element={<DeleteBooks/>} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBooks />} />
+      <Route path='/books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
     </Routes>
   )
 }
